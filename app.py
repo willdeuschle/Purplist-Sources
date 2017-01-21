c8328f5480c5a47a7a12b107ca6872ef52d9adbc
willdeuschle <wjdeuschle@gmail.com>
2017-01-21 08:48:23 -0500

    bug fixed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config.from_object(os.environ(['APP_SETTINGS']))
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 print(os.environ['APP_SETTINGS'])
 
